db93f5dd2f92ccab04b3ec4c912151ac3ba37639
deepa-kothandaraman28 <deeparamu7667@gmail.com>
2026-06-25 13:17:50 +0530

    Add files via upload

---
+++ WEEK_2/Spring Core_Maven/Exercise 4 Creating and Configuring a Maven Project/LibraryManagement/src/main/java/com/library/test/TestSpring.java
@@ -0,0 +1,11 @@
+package com.library.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+
+    public static void main(String[] args) {
+        System.out.println("Spring Import Success");
+    }
+}
